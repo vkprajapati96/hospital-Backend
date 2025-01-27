e9cce979e1e6d721bc07fe4b575170ef4599ad5b
vishal <141538917+vkprajapati96@users.noreply.github.com>
2025-01-27 15:22:05 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -2,16 +2,18 @@ export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
   const cookieName = user.role === 'Admin' ? 'adminToken' : 'patientToken';
 
+  const isProduction = process.env.NODE_ENV === 'production';
 
   res
     .status(statusCode)
-  res.cookie(cookieName, token, {
-  expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-  httpOnly: true,
-  secure: process.env.NODE_ENV === 'production', // Secure cookies for HTTPS
-  sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // 'None' for cross-origin
-});
-
+    .cookie(cookieName, token, {
+      expires: new Date(
+        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true, // Prevent client-side access
+      secure: isProduction, // Only set in HTTPS
+      sameSite: isProduction ? 'None' : 'Lax', // 'None' for cross-origin
+    })
     .json({
       success: true,
       message,
