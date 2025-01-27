261495d7e020d4053bc4cd9a123bf25ae081d326
vishal <141538917+vkprajapati96@users.noreply.github.com>
2025-01-27 15:13:19 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,12 +5,13 @@ export const generateToken = (user, message, statusCode, res) => {
 
   res
     .status(statusCode)
-    .cookie(cookieName, token, {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
-            // Prevent CSRF
-    })
+  res.cookie(cookieName, token, {
+  expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production', // Secure cookies for HTTPS
+  sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // 'None' for cross-origin
+});
+
     .json({
       success: true,
       message,
